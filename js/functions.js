01295235064f6fee3d477bd1f5fa9f8e4f701ef9
danielgolden <danielgolden90@gmail.com>
2022-05-20 14:39:32 -0400

    Trying out blotter

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,85 @@
-document.addEventListener("DOMContentLoaded", () => {
+// import Blotter from "./blotter.min.js";
+
+window.addEventListener("DOMContentLoaded", () => {
   initApp();
 });
 
+window.addEventListener("ready", () => {
+  const $canvas = document.querySelector("canvas");
+  console.log($canvas);
+  // $canvas.width = 100;
+});
+
 const initApp = () => {
-  // Make an instance of two and place it on the page
-  const params = { fitted: true };
-  const $elem = document.querySelector(".animated-border");
-  const two = new Two(params).appendTo($elem);
+  WebFont.load({
+    google: {
+      families: ["IBM Plex Sans"],
+    },
+    active: () => createBlotterText(),
+  });
 
-  const rectWidth = 1000;
-  const rectHeight = 500;
-  const rect = two.makeRectangle(0, 0, rectWidth, rectHeight);
-  rect.fill = "rgb(127, 127, 127)";
-  rect.noStroke();
+  let textColor = "#202527";
 
-  two.update();
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    textColor = "#202527";
+  }
+
+  const createBlotterText = () => {
+    // BLOTTER - Example 2
+    var text = new Blotter.Text("Daniel Golden", {
+      family: "'IBM Plex Sans', serif",
+      size: 34,
+      fill: textColor,
+      paddingTop: 0,
+    });
+
+    console.log("text.fill", text.properties.fill);
+    var material = new Blotter.ChannelSplitMaterial();
+
+    // Flies Matieral
+    // material.uniforms.uPointCellWidth.value = 0.02;
+    // material.uniforms.uPointRadius.value = 0.8;
+    // material.uniforms.uSpeed.value = 10;
+
+    // Liquid Distort
+    // material.uniforms.uSpeed.value = 0.25;
+
+    // Channel Split
+    material.uniforms.uOffset.value = -0.05;
+    material.uniforms.uApplyBlur.value = 1;
+    material.uniforms.uAnimateNoise.value = true;
+
+    // Rolling distort
+    // material.uniforms.uSineDistortSpread.value = 0.1;
+    // material.uniforms.uSineDistortCycleCount.value = 2;
+    // material.uniforms.uSineDistortAmplitude.value = 0.01;
+    // material.uniforms.uNoiseDistortVolatility.value = 10;
+    // material.uniforms.uNoiseDistortAmplitude.value = 0.002;
+    // material.uniforms.uRotation.value = 176;
+    // material.uniforms.uSpeed.value = 0.35;
+
+    var blotter = new Blotter(material, {
+      texts: text,
+    });
+
+    var $elem = document.querySelector(".blotter-text");
+    var scope = blotter.forText(text);
+
+    scope.appendTo($elem);
+    Blotter.addEventListener("ready", () => {
+      console.log("hi");
+    });
+
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        text.properties.fill = newColorScheme === "dark" ? "#fff" : "#202527";
+        blotter.needsUpdate = true;
+      });
+  };
 };
